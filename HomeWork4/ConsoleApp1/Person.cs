9a5f801b0f8a9d9a9b03f7f876ffe2e7f36cfbab
Vladosss <dasistperfektosss@gmail.com>
2023-05-29 18:02:40 +0200

    HomeWork4

---
+++ HomeWork4/ConsoleApp1/Person.cs
@@ -0,0 +1,91 @@
+﻿namespace ConsoleApp1;
+
+public class Person
+{
+    
+        private string _name;
+        private DateTime _birthDate;
+        
+        DateTime year=DateTime.Today;
+        public string Name { get { return _name; }}
+        public DateTime BirthDate { get { return _birthDate; } }
+
+        public Person()
+        {
+            
+        }
+
+        public Person(string name, DateTime birthDate)
+        {
+            _name = name;
+            _birthDate = birthDate;
+        }
+
+        public void CalculateAge()
+        {
+            int currentYear = year.Year;
+            int yearOfPerson = currentYear - _birthDate.Year;
+            Console.WriteLine($"My year is {yearOfPerson}");
+        }
+
+        public int GetAge()
+        {
+            return year.Year - _birthDate.Year;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter person name");
+            string personName = Console.ReadLine();
+
+            Console.WriteLine("Enter person year of birth");
+            string personYear = Console.ReadLine();
+
+            Console.WriteLine("Enter person month of birth");
+            string personMonth = Console.ReadLine();
+
+            Console.WriteLine("Enter person day of birth");
+            string personDay = Console.ReadLine();
+
+            if (!int.TryParse(personYear,out var year) ||!int.TryParse(personMonth,out var month)
+                || !int.TryParse(personDay,out var day))
+            {
+                Console.WriteLine("Error,please enter symbols");
+            }
+            else
+            {
+                _name = personName;
+                _birthDate = new DateTime(year, month, day);
+                
+                Console.WriteLine("Entered data!");
+            }
+        }
+
+        public void ChangeName()
+        {
+            Console.WriteLine($"Your name is {_name},enter new name to change");
+            string newName = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(newName))
+            {
+                _name = newName;
+                Console.WriteLine("You change name!");
+            }
+            else
+            {
+                Console.WriteLine("Error,please enter a value");
+            }
+          
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {_name}, Birth Date: {_birthDate.ToShortDateString()}";
+        }
+
+        public void OutPut()
+        {
+            Console.WriteLine($"My name is {_name} ,My birthday is {_birthDate.Day},birthmonth {_birthDate.Month}," +
+                              $"bithyear {_birthDate.Year}");
+        }
+}
---
+++ HomeWork4/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Channels;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime person1DateTime = new DateTime(2010, 08, 10);
+            Person person1 = new Person("Vlad",person1DateTime);
+            
+            DateTime person2DateTime = new DateTime(2008, 12, 11);
+            Person person2 = new Person("Roma",person2DateTime);
+            
+            DateTime person3DateTime = new DateTime(1995, 01, 15);
+            Person person3 = new Person("Vlad",person3DateTime);
+            
+            DateTime person4DateTime = new DateTime(1976, 10, 29);
+            Person person4 = new Person("Vlad",person4DateTime);
+            
+            DateTime person5DateTime = new DateTime(2013, 12, 17);
+            Person person5 = new Person("Vlad",person5DateTime);
+            
+            DateTime person6DateTime = new DateTime(2010, 07, 02);
+            Person person6 = new Person("Vlad",person6DateTime);
+
+            var allPersons = new List<Person>() { person1, person2, person3, person4, person5, person6 };
+
+            foreach (var person in allPersons)
+            {
+                Console.WriteLine("My name is" + person.Name);
+                person.CalculateAge();
+
+                if (person.GetAge()<=16)
+                {
+                    person.ChangeName();
+                }
+
+                Console.WriteLine();
+            }
+            
+            FindSameNames(allPersons);
+            
+        }
+
+        private static void FindSameNames(List<Person> persons)
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                for (int j = i+1; j < persons.Count; j++)
+                {
+                    if (persons[i].Name==persons[j].Name)
+                    {
+                        Console.WriteLine("Person with same name");
+
+                        Console.WriteLine( persons[i].ToString());
+                        Console.WriteLine(persons[j].ToString());
+                    }
+                }
+            }
+        }
+    }
+}
+
+  
+
+
